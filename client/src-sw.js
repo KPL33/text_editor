53d13001f7b851c9d71b214bd8745d1099582b85
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-18 13:40:58 -0700

    error resolved in src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,10 +1,12 @@
+//Here, we "require" (import) various modules to help with the functionality of "cache"ing data that the app receives, in order to retrieve it when the app is being used "offline."
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+//Here, "precache" assets specified in the WorkBox ("WB") "MANIFEST" and provide routes for static assets, making them available on page-load via the "warmStrategyCache" function. We also establish "statuses" to indicate if the "cache"ing was successful and provide an "Expiration" "maxAge" in "seconds" for this "Plugin" (30 days x 24 hours x 60 min x 60 seconds).
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -24,16 +26,15 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+//Here, we allow the app to "navigate" to a new "page", if the "Cache" is available.
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+//Here, we register a route for asset requests "style" (stylesheets), "script" (various scripts), and (service) "workers". Utilizing the "StaleWhileRevalidate" strategy, it serves assets from the cache while revalidating them in the background. Assets are stored in the "asset-cache". The "CacheableResponsePlugin" contains status codes, similar to its functionality within the "preCacheAndRoute" shown above.
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
