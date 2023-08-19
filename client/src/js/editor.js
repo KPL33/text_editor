b8bcef297e051561271d05536385bcf764e1d06b
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-19 14:36:34 -0700

    Notes added to editor and header.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,16 +1,18 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
+//Here, we import "get" and "put" methods detailed in the "./database.js" file and import the "header" constant from "./header.js".
 import { getDb, putDb } from './database';
 import { header } from './header';
 
+//When "Data" is received in "localStorage", a "class" is created via the "constructor" method. We then establish that "localData" will store the "Item" ("content"), after "get"ting it from "localStorage".
 export default class {
   constructor() {
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    //"CodeMirror" is a JavaScript library that provides a text-editor for web apps. This snippet checks that the "CodeMirror" library is loaded and "throw"s an "Error" in the event that it is not loaded.
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
-    }
+    };
 
+    //Here, we create an instance of "CodeMirror" and targets the element with ID "main" in our html. The instance has its various important keys and their values established below, to set preferences for how CodeMirror will operate in our app.
     this.editor = CodeMirror(document.querySelector('#main'), {
       value: '',
       mode: 'javascript',
@@ -22,21 +24,21 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    //When the "editor" is ready, if available, "data" is rertieved from the the "IndexedDB". If none is available, "localData" stored in "localStorage" is loaded, as a fall-back option. If no "data" is present in either of those options, the contents of "header" (which we "import"ed at the beginning of this document) will load, greeting the user with a "start-screen", of sorts.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
     });
 
+    //When the data in "localStorage" "change"s, the new "Value" is set to show in the text-"editor".
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    //"blur" here allows our "editor" to listen for the moment When the "editor" is no longer in "focus". At that point, the message shown "log"s to the "console" and the new "content" entered by the user is "put" into the "localStorage" "DB".
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
     });
-  }
-}
+  };
+};
--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,3 +1,4 @@
+//Here, we have a "logo" that will greet the user, when the app is intialized. Once it has been installed, or if user-entered text is present in the database, that text will appear, instead of this "header".
 const header = `
 /*
        _____  ____________
