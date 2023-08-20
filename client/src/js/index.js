063a5427cc1bf608e9204ed65ce65e69f53d07db
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-20 14:41:34 -0700

    Resolving bugs

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,11 +1,14 @@
+//Here, we "import" various modules that will help our app operate.
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
+import { header } from './header';
 
 const main = document.querySelector('#main');
-main.innerHTML = '';
+main.innerHTML = header;
 
+//"loadSpinner" is defined to create and append a loading "Spinner" "Element" to the "main" container. It prepares a "Spinner" that will be shown while the "editor" is being initialized.
 const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
@@ -17,13 +20,14 @@ const loadSpinner = () => {
   main.appendChild(spinner);
 };
 
+//Here, we create an instance of the "Editor" class (the particulars of this class having been established in "./editor.js" and imported above). It gives us access to the functionality of the code editor for our app. "if" it fails to initialze, the "Spinner" that we established above "load"s instead.
 const editor = new Editor();
 
 if (typeof editor === 'undefined') {
   loadSpinner();
 }
 
-// Check if service workers are supported
+//Here, our app verifies that the web browser ("navigator") supports "serviceWorker"s. If it does not, an "error" logs to the "console" stating this.
 if ('serviceWorker' in navigator) {
   // register workbox service worker
   const workboxSW = new Workbox('/src-sw.js');
